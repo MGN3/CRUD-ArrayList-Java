ba0076cf4b48ffa6abd051e75e595fb306909433
Marcelino Gil Nombela <112982522+MGN3@users.noreply.github.com>
2023-04-21 18:54:20 +0200

    Update ArrayListUtilities.java
    The method includes a try catch now.

--- src/ArrayListUtilities.java
+++ src/ArrayListUtilities.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 
 public class ArrayListUtilities {
 
-	/** This method shows the first attribute(name) of each Technology */
+	/**
+	 * This method shows the first attribute(name) of each Technology
+	 * 
+	 * @param ArrayList The ArrayList of Technology objects
+	 */
 	public static void showNames(ArrayList<Technology> ArrayList) {
-		int arraySize = ArrayList.size();
+		try {
+			int arraySize = ArrayList.size();
 
-		for (int i = 0; i < arraySize; i++) {
-			System.out.println("Tech number " + (i + 1) + ": " + ArrayList.get(i).getTechName());
+			for (int i = 0; i < arraySize; i++) {
+				System.out.println("Tech number " + (i + 1) + ": " + ArrayList.get(i).getTechName());
 
+			}
+		} catch (NullPointerException e) {
+			// In this catch I show different messages.
+			System.out.println("NullPointerException caught: " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Exception caught: " + e.getMessage());
+			e.printStackTrace();
 		}
+
 	}
 
 }
