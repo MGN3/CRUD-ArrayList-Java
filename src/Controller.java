a67e36f8c294a662355dc28affcc7b5ca241ad65
Marcelino Gil Nombela <112982522+MGN3@users.noreply.github.com>
2023-04-21 18:58:26 +0200

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -2,9 +2,6 @@ package practica2_MarcelinoGil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import javax.swing.JOptionPane;
 
 public class Controller {
 	/**
@@ -17,12 +14,6 @@ public class Controller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// JOptionPane.showMessageDialog(null, "Mensaje a mostrar");
-
-		// WebPage blog = new WebPage(Utilities.getString("mgnblog"), "mgnblog.com",
-		// 400, null);
-		// WebPage.createTechnologies(2);
-
 		// Selector for the main menu.
 		int mainMenuChoice;
 
@@ -33,43 +24,17 @@ public class Controller {
 		char doSomethingElse;
 
 		// Variable to save a specific index of the ArrayList<Technology>.
-		int arrayListIndex;
-
-		// Multipurpose boolean.
-		boolean flag;
-
-		// ------------------------------------------
-		Technology foundTech;
-
-		int attributeSelector;
+		// int arrayListIndex;
 
 		// The main object of this program: a webpage. Set null and without a specific
 		// ArrayList<Technology>.
-		WebPage ecommerce = new WebPage(null, null, 0);
+		WebPage myWebsite = new WebPage(null, null, 0);
 
 		// This is the ArrayList of technologies that stores all the premade
 		// technologies bellow.
 		ArrayList<Technology> coreStack = new ArrayList<Technology>();
 
 		// Premade Technologies.
-		/*
-		 * Technology mysql = new Technology("MySQL", "DBMS", "Store data", 1995,
-		 * "MIT"); Technology mongodb = new Technology("MongoDB", "DBMS", "Store data",
-		 * 0, "MIT123123"); Technology sqlserver = new Technology("SQL", "bbdd",
-		 * "almacenar informacion", 0, null); Technology java = new Technology("SQL",
-		 * "bbdd", "almacenar informacion", 0, null); Technology javascript = new
-		 * Technology("SQL", "bbdd", "almacenar informacion", 0, null); Technology
-		 * typescript = new Technology("SQL", "bbdd", "almacenar informacion", 0, null);
-		 * Technology reactjs = new Technology("SQL", "bbdd", "almacenar informacion",
-		 * 0, null); Technology nodejs = new Technology("SQL", "bbdd",
-		 * "almacenar informacion", 0, null); Technology html = new Technology("SQL",
-		 * "bbdd", "almacenar informacion", 0, null); Technology css = new
-		 * Technology("SQL", "bbdd", "almacenar informacion", 0, null); Technology
-		 * bootstrap = new Technology("SQL", "bbdd", "almacenar informacion", 0, null);
-		 * Technology tailwindCss = new Technology("SQL", "bbdd",
-		 * "almacenar informacion", 0, null); Technology springboot = new
-		 * Technology("SQL", "bbdd", "almacenar informacion", 0, null);
-		 */
 		Technology mysql = new Technology("MySQL", "DBMS", "Store data", 1995, "MIT");
 		Technology mongodb = new Technology("MongoDB", "DBMS", "Store data", 2009, "GNU");
 		Technology sqlserver = new Technology("SQL Server", "DBMS", "Store data", 1989, "EULA");
@@ -92,17 +57,10 @@ public class Controller {
 		coreStack.addAll(Arrays.asList(mysql, mongodb, sqlserver, java, javascript, typescript, reactjs, nodejs, html,
 				css, bootstrap, tailwindCss, springboot));
 
-		// System.out.println(webList.toString());
-
-		// System.out.println(webList.get(0));
-
-		// webList.toString().contains("bab");
-
-		// webList.add(new WebPage(Utilities.getString("introduce Name "), null, 0,
-		// prueba));
-
 		// Before the CRUD menu appears, the WebPage main CRUD object must be created:
-		ecommerce = new WebPage(Utilities.getString("Introduce Webpage name: "),
+		System.out.println("\u001B[107m" + "\u001B[32m" + "Welcome, please introduce basic data about the Webpage");
+		System.out.print("\u001B[0m");
+		myWebsite = new WebPage(Utilities.getString("Introduce Webpage name: "),
 				Utilities.getString("Introduce the URL: "),
 				Utilities.getIntBetween(1, 999999999, "Introduce the budget for the web project: "));
 
@@ -127,15 +85,15 @@ public class Controller {
 				// ------------ CREATE ------------
 
 				// Request and return the desired option
-				menuChoice = Utilities.getIntBetween(1, 5,
-						"Press '1' to create/rewrite the WebPage data. \nPress '2' to add a new tech. \nPress '3' to mostrar todo. \nPress '4' to add an existing tech. \nPress '5' to exit.");
+				menuChoice = Utilities.getIntBetween(1, 4,
+						"Press '1' to create/overwrite the WebPage data. \nPress '2' to add a new tech. \nPress '3' Show and add premade Techs. \nPress '4' to exit submenu.");
 
 				switch (menuChoice) {
 
 				case 1:
-					// Option to create/rewrite the main object of this CRUD: a webpage.
-					// WebPage.
-					ecommerce = new WebPage(Utilities.getString("Introduce Webpage name: "),
+					// Option to create/overwrite the main object of this CRUD: a webpage.
+					// It also gives the choice to include all the premade Technologies.
+					myWebsite = new WebPage(Utilities.getString("Introduce Webpage name: "),
 							Utilities.getString("Introduce the URL: "),
 							Utilities.getIntBetween(1, 999999999, "Introduce the budget for the web project: "));
 
@@ -143,7 +101,7 @@ public class Controller {
 					doSomethingElse = Utilities.getCharMenu('y', 'n',
 							"Do you want to add every premade Technology in the WebPage?");
 					if (doSomethingElse == 'y') {
-						ecommerce.setTechStack(coreStack);
+						myWebsite.setTechStack(coreStack);
 					} else {
 						System.out.println("The list of technologies will remain empty");
 					}
@@ -151,44 +109,67 @@ public class Controller {
 
 				case 2:
 					// To add a new Technology into WebPage's ArrayList of Technologies with user
-					// imput for each Technology attribute.
-					ecommerce.addTechnology();
+					// input for each Technology attribute.
+					myWebsite.addTechnology();
 
 					break;
 
 				case 3:
-					// Add one or more technologies into the WebPage ArrayList<Technology>.
+					// Adds one or more premade technologies into the WebPage ArrayList<Technology>.
 
-					// Do-while loop to add another Technology until imput is 'n'
-					do {
-						// First, we show all the availeable premade Technolgies.
-						ArrayListUtilities.showNames(coreStack);
-
-						/*
-						 * "flag" prevents the same technology from being added again. IMPORTANT: 1 is
-						 * subtracted from Utilities.getIntBetween imput to match the proper position of
-						 * the ArrayList.
-						 */
-						flag = ecommerce.getTechStack().contains(coreStack.get(arrayListIndex = Utilities
-								.getIntBetween(1, 13, "Enter the number of the tech you want to add?") - 1));
-
-						if (!flag) {
-							System.out.println("The following Technology have been added: '"
-									+ coreStack.get(arrayListIndex).getTechName());
-							ecommerce.addTechnology(coreStack.get(arrayListIndex));
-
-						} else {
-							System.out.println("This Technology has already been added.");
-						}
-
-						doSomethingElse = Utilities.getCharMenu('y', 'n', "Do you want to add another Technology?");
-
-					} while (doSomethingElse != 'n');
+					myWebsite.addPremadeTech(coreStack);
 
 					break;
 
 				case 4:
+					// Exits sub menu
+					break;
 
+				default:
+					// I don't find any chance of getting an invalid value. But just in case.
+					break;
+				}
+
+				// The user is given the option to execute another action.
+				doSomethingElse = Utilities.getCharMenu('y', 'n', "Do you want to do something else?");
+				if (doSomethingElse == 'n') {
+					mainMenuChoice = 5;
+				}
+				break;
+
+			case 2:
+				// ------------ READ ------------
+
+				// Request and return the desired option
+				menuChoice = Utilities.getIntBetween(1, 5,
+						"Press '1' to find the first match. \nPress '2' to find all the matches. \nPress '3' to show all the Web data. \nPress '4' to search by category. \\nPress '5' to exit submenu");
+				switch (menuChoice) {
+
+				case 1:
+					// Shows the first match found. findFirstMatch returns and int but collecting it
+					// is not usefull in this case.
+					myWebsite.findFirstMatch(Utilities.getString("Introduce the word you want to find"));
+					break;
+
+				case 2:
+					// Shows all the matches found.
+					myWebsite.findAllMatches(Utilities.getString("Introduce the word you want to find"));
+					break;
+
+				case 3:
+					// Call to predefined toString method for the main WebPage object
+					System.out.println(myWebsite.toString());
+					break;
+
+				case 4:
+					/*
+					 * Same method but applying overload, in this case, the parameters are the data
+					 * request and the category/attribute to fetch the requested data. The
+					 * output/return from findFirstMatch is not needed so it wont be collected.
+					 */
+					myWebsite.findFirstMatch(Utilities.getString("What do you want to find: "), Utilities.getIntBetween(
+							1, 5,
+							"Enter 1, 2, 3, 4 or 5 to search by: 1-Name, 2-Tech type, 3-Purpose, 4-Release year or 5-License"));
 					break;
 
 				case 5:
@@ -207,111 +188,34 @@ public class Controller {
 				}
 				break;
 
-			case 2:
-				// ------------ READ ------------
-
-				// Request and return the desired option
-				menuChoice = Utilities.getIntBetween(1, 4,
-						"Press '1' to find the first coincidence. \nPress '2' to find all the coincidences. \nPress '3' to show the whole database. \nPress '4' to exit");
-				switch (menuChoice) {
-
-				case 1:
-					// Shows the first match found. Despite the fact that it returns an int, in this
-					// case it is not necessary to collect it.
-					ecommerce.findFirstMatch(Utilities.getString("Introduce the word you want to find"));
-					break;
-
-				case 2:
-					// Shows all the matches found.
-					ecommerce.findAllMatches(Utilities.getString("Introduce the word you want to find"));
-					break;
-
-				case 3:
-					// Call to predefined toString method for the main WebPage object
-					System.out.println(ecommerce.toString());
-					break;
-
-				case 4:
-					// Search only a specific attribute of the Technology object. In this case,
-					// .findFirstMatch return is nos usefull so no variable will collect it.
-					ecommerce.findFirstMatch(Utilities.getString("What do you want to find: "), Utilities.getIntBetween(
-							1, 5,
-							"Enter 1, 2, 3, 4 or 5 to search by: 1-Name, 2-Tech type, 3-Purpose, 4-Release year or 5-License"));
-					break;
-///////////////AÑADIR EXIT OPTION
-				default:
-					// I don't find any chance of getting an invalid value. But just in case.
-					break;
-				}
-
-				// The user is given the option to execute another task on the database.
-				doSomethingElse = Utilities.getCharMenu('y', 'n', "Do you want to do something else?");
-				if (doSomethingElse == 'n') {
-					mainMenuChoice = 5;
-				}
-				break;
-
 			case 3:
 				// ------------ UPDATE ------------
 
-				// Request and return the desired option
+				// Requests and returns the desired option.
 				menuChoice = Utilities.getIntBetween(1, 3,
-						"Press '1' to update a specific row. \nPress '2' to update all the matches for your data request\nPress '3' to exit.");
+						"Press '1' to update a Technology. \nPress '2' to update a specific data of the Technology. \nPress '3' to exit submenu.");
 				switch (menuChoice) {
 
 				case 1:
-					// To overwrite Technology's attributes after a match.
-					arrayListIndex = ecommerce
-							.findFirstMatch(Utilities.getString("What do you want to find and update?"));
+					// To overwrite Technology's attributes where the first match is found.
 
-					// If the value of arrayListIndex is -1, no matches were found.
-					if (arrayListIndex > -1) {
-						ecommerce.getTechStack().set(arrayListIndex,
-								new Technology(Utilities.getString("Set new name:"),
-										Utilities.getString("Set new tech type:"),
-										Utilities.getString("Set new purpose:"),
-										Utilities.getIntBetween(1900, 2050, "Set new release year:"),
-										Utilities.getString("Set new license:")));
-					}
+					/**
+					 * Calling the method with the same object that is going to be used as a
+					 * parameter seems wierd but it was an easier way to reuse the object method:
+					 * ".findFirstMatch()".
+					 */
+					myWebsite.updateTech(myWebsite);
+
 					break;
 
 				case 2:
-					// This option is meant to change a single attribute of the Technology where the
-					// data requested has first been found.
-					arrayListIndex = ecommerce.findFirstMatch(Utilities.getString("What do you want to find: "),
-							attributeSelector = Utilities.getIntBetween(1, 5,
-									"Enter 1, 2, 3, 4 or 5 to search by: 1-Name, 2-Tech type, 3-Purpose, 4-Release year or 5-License"));
+					/*
+					 * This option changes a single attribute of the Technology where the data
+					 * requested has first been found.
+					 * 
+					 */
+					myWebsite.updateTechAttribute(myWebsite);
 
-					// If the value of arrayListIndex is -1, no matches were found.
-					if (arrayListIndex > -1) {
-
-						switch (attributeSelector) {
-
-						case 1:
-							ecommerce.getTechStack().get(arrayListIndex)
-									.setTechName(Utilities.getString("Set new name:"));
-							break;
-						case 2:
-							ecommerce.getTechStack().get(arrayListIndex)
-									.setTechType(Utilities.getString("Set new tech type:"));
-							break;
-						case 3:
-							ecommerce.getTechStack().get(arrayListIndex)
-									.setPurpose(Utilities.getString("Set new purpose:"));
-							break;
-						case 4:
-							ecommerce.getTechStack().get(arrayListIndex)
-									.setReleaseYear(Utilities.getIntBetween(1900, 2050, "Set new release year:"));
-							break;
-						case 5:
-							ecommerce.getTechStack().get(arrayListIndex)
-									.setLicense(Utilities.getString("Set new license:"));
-							break;
-						default:
-							System.out.println("Invalid option");
-							break;
-						}
-					}
 					break;
 
 				case 3:
@@ -335,26 +239,22 @@ public class Controller {
 
 				// Request and return the desired option.
 				menuChoice = Utilities.getIntBetween(1, 3,
-						"Press '1' to remove a row. \nPress '2' to remove all the matches. \nPress '3' to remove every field. \nPress '4' to restart the database. \nPress '5' to exit.");
+						"Press '1' to remove the first match found. \nPress '2' to remove the entire list of Technologies. \nPress '3' to exit submenu.");
 				switch (menuChoice) {
 
 				case 1:
 					// Removes a specific Technology out of the ArrayList.
-					arrayListIndex = ecommerce
-							.findFirstMatch(Utilities.getString("Which Technology do you want to find and remove?"));
+					myWebsite.remove(myWebsite);
 
-					if (arrayListIndex > -1) {
-						ecommerce.getTechStack().remove(arrayListIndex);
-						System.out.println("The Technology have been removed");
-					}
 					break;
 
 				case 2:
 					// Removes all the Technologies objects inside the ArrayList.
 					doSomethingElse = Utilities.getCharMenu('y', 'n',
 							"Are you sure? This will remove every Technology from the list");
+
 					if (doSomethingElse == 'y') {
-						ecommerce.removeArrayList();
+						myWebsite.remove();
 					}
 					break;
 
@@ -362,6 +262,7 @@ public class Controller {
 					// Exit option
 
 					break;
+
 				default:
 					// I don't find any chance of getting an invalid value. But just in case.
 					break;
